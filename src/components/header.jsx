0dc90cbf385afcf9936d27e436045a753a22a66e
Aaron Cil <cilaaron27@gmail.com>
2022-12-28 20:02:19 -0500

    initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+return (
+    <header>
+        <p>Thuy's Page</p>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Random</li>
+        </ul>
+    </header>
+);
+}
+
+export default Header;
